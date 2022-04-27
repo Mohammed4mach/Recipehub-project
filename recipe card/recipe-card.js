c02c6bf3f23ac7ba17470b9a1c2de5690a28d00c
Sohila25Akram <103384154+Sohila25Akram@users.noreply.github.com>
2022-04-28 00:15:35 +0200

    Update recipe-card.js

--- recipe card/recipe-card.js
+++ recipe card/recipe-card.js
@@ -1,12 +1,12 @@
-let nextButton = document.getElementById("next"),
+let nextButton = document.getElementById("next1"),
 
-    prevButton = document.getElementById("prev"),
+    prevButton = document.getElementById("prev1"),
 
-    sliderCon = document.getElementById("cards-container"),
+    sliderCon = document.getElementById("cards-container-id"),
 
-    ButtonOne = document.getElementById('one'),
+    ButtonOne = document.getElementById('one-id'),
 
-    ButtonSix = document.getElementById('six');
+    ButtonSix = document.getElementById('six-id');
 
 
 sliderCon.style.left = "0";
@@ -38,7 +38,7 @@ nextButton.onclick=ButtonSix.onclick=function()
 
 prevButton.onclick=ButtonOne.onclick=function()
 {
-
+    
     sliderCon.style.left = 0;
 
     prevButton.classList.add('disabled');
@@ -51,122 +51,3 @@ prevButton.onclick=ButtonOne.onclick=function()
 
     ButtonOne.style.backgroundColor = "rgb(255, 122, 55)";
 }
-
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-
-/*var cards = Array.from(document.querySelectorAll(".card"));
-var slidesCount = cards.length;
-var currentSlide = 1;
-
-prevButton.onclick = prevSlide;
-nextButton.onclick = nextSlide;
-
-for(var i=1; i<=slidesCount; i++){
-
-    var paginationItem = document.createElement('div');
-
-    paginationItem.setAttribute('data-index', i);
-
-    document.getElementById('slider2').appendChild(paginationItem);
-}
-
-document.getElementById('slider2').appendChild(paginationItem);
-
-var paginationCreatedUl = document.getElementById('slider2');
-
-var paginationBullets = Array.from(document.querySelectorAll('#slider2 div'));
-
-
-for(var i = 0; i < paginationBullets.length; i++){
-
-    paginationBullets[i].onclick = function(){
-        
-        currentSlide = parseInt(this.getAttribute('data-index'));
-
-        theChecker();
-
-    }
-
-}
-
-
-function theChecker(){
-
-    removeAllActive();
-
-    cards[currentSlide-1].classList.add('active');
-
-    paginationCreatedUl.children[currentSlide-1].classList.add('active');
-
-    if(currentSlide == 1){
-            
-        prevButton.classList.add('disabled');
-        
-    }else{
-
-        prevButton.classList.remove('disabled');
-
-    }
-
-    if(currentSlide == slidesCount){
-
-        nextButton.classList.add('disabled');
-
-    }else{
-
-        nextButton.classList.remove('disabled');
-
-    }
-
-}
-
-theChecker();
-
-function removeAllActive() {
-    
-    cards.forEach(function (card){
-
-        card.classList.remove('active');
-
-    });
-    
-
-    paginationBullets.forEach(function (bullet){
-
-        bullet.classList.remove('active');
-
-    });
-
-}
-
-function nextSlide(){
-    
-    if(nextButton.classList.contains('disabled')){
-        
-        return false;
-
-    }else{
-
-        currentSlide++;
-
-        theChecker();
-
-    }
-}
-
-function prevSlide(){
-
-    if(prevButton.classList.contains('disabled')){
-        
-        return false;
-
-    }else{
-
-        currentSlide--;
-
-        theChecker();
-
-    }
-}*/
-
